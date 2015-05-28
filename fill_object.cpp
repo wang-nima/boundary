efc41804f99240db007fcd90fc4b36882532d679
shawn <jak47042007@163.com>
2015-05-28 13:21:27 -0700

    fill boundary

--- fill_object.cpp
+++ fill_object.cpp
@@ -6,7 +6,8 @@
 #include <cstdlib>
 using namespace std;
 
-#define CELL_LENGTH 1.0		// 0.1 mm
+#define CELL_LENGTH 3		// 1 mm
+#define SMALL_CELL_LENGTH 0.1
 
 
 #define UNLABELED 0
@@ -29,18 +30,20 @@ void init() {
 }
 
 
-// this function is extreamly slow, better to use kd-tree
-bool point_exist_in_cell(float lowerleft_x, float lowerleft_y) {
+// this function is extreamly and naive slow, better to use kd-tree
+bool point_exist_in_cell(float lowerleft_x, float lowerleft_y, int all, float length) {
+	int count = 0;
+
 	for (int i = 0; i < v.size(); i++) {
 		Point p = v[i];
 		if (p.x > lowerleft_x &&
-			p.x < lowerleft_x + CELL_LENGTH &&
+			p.x < lowerleft_x + length &&
 			p.y > lowerleft_y &&
-			p.y < lowerleft_y + CELL_LENGTH) {
-			return true;
+			p.y < lowerleft_y + length ) {
+			count++;
 		}
 	}
-	return false;
+	return count >= all ? true : false;
 }
 
 // inside points and boundary are both neighbors
@@ -155,7 +158,7 @@ int main() {
 	vector<vector<unsigned int> > grid(m, vector<unsigned int>(n, UNLABELED));
 	for (float i = 0; i < m; i++) {
 		for (float j = 0; j < n; j++) {
-			if (point_exist_in_cell(min_x + i * CELL_LENGTH, min_y + j * CELL_LENGTH)) {
+			if (point_exist_in_cell(min_x + i * CELL_LENGTH, min_y + j * CELL_LENGTH, 10, CELL_LENGTH)) {
 				grid[i][j] |= BOUNDARY;
 				cout << min_x + i * CELL_LENGTH << " " << min_y + j * CELL_LENGTH << endl;
 			}
@@ -279,18 +282,30 @@ int main() {
 
 	//grid = grow(grid);
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 1000; i++) {
 		grid = grow(grid, 5, true);
 	}
 
-	for (int i = 0; i < 30; i++) {
+	//for (int i = 0; i < 1000; i++) {
+	//	grid = shrink(grid, 5);
+	//}
+
+
+	for (int i = 0; i < 300; i++) {
 		grid = grow(grid, 3, false);
 	}
 
-	//for (int i = 0; i < 10; i++) {
-	//	grid = shrink(grid);
-	//}
+// if a grid is boundary it can't be inside
+ 	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((grid[i][j] & BOUNDARY) == BOUNDARY) {
+				grid[i][j] &= ~INSIDE;
+			}
+		}
+	}
 
+// print random inside points
+	
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
 			if ((grid[i][j] & INSIDE) == INSIDE) {
@@ -298,6 +313,104 @@ int main() {
 			}
 		}
 	}
+
+	
+	freopen("final.txt", "w", stdout);
+
+
+// print fine boundary
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((grid[i][j] & BOUNDARY) == BOUNDARY) {
+				// look for inside grid
+				// 1 up 2 right 3 down 4 left 0 not found
+				int direction = 0;
+				if (j + 1 < n && (grid[i][j+1] & INSIDE) == INSIDE) {
+					direction = 1;
+				} else if (i + 1 < m && (grid[i+1][j] & INSIDE) == INSIDE) {
+					direction = 2;
+				} else if (j - 1 >= 0 && (grid[i][j-1] & INSIDE) == INSIDE) {
+					direction = 3;
+				} else if (i - 1 >= 0 && (grid[i-1][j] & INSIDE) == INSIDE) {
+					direction = 4;
+				}
+				// inside grid near me not found
+				if (direction == 0) {
+					continue;
+				}
+				int size = CELL_LENGTH / SMALL_CELL_LENGTH;
+				vector<vector<bool> > exist(size, vector<bool>(size, false));
+				// cut big cell into smaller
+				for (int p = 0; p < size; p++) {
+					for (int q = 0; q < size; q++) {
+						if (point_exist_in_cell(min_x + i * CELL_LENGTH + p * SMALL_CELL_LENGTH,
+												min_y + j * CELL_LENGTH + q * SMALL_CELL_LENGTH,
+												2,
+												SMALL_CELL_LENGTH)) {
+							exist[p][q] = true;
+						}
+					}
+				}
+
+				if (direction == 1) {
+					// from top to buttom
+					for (int p = 0; p < size; p++) {
+						for (int q = size - 1; q >= 0; q--) {
+							if (exist[p][q] == false) {
+								cout << min_x + i * CELL_LENGTH + p * SMALL_CELL_LENGTH << " "\
+									 << min_y + j * CELL_LENGTH + q * SMALL_CELL_LENGTH << endl;
+							} else {
+								break;
+							}
+						}
+					}
+				} else if (direction == 2) {
+					// from right to left
+					for (int q = size - 1; q >= 0; q--) {
+						for (int p = size - 1; p >= 0; p--) {
+							if (exist[p][q] == false) {
+								cout << min_x + i * CELL_LENGTH + p * SMALL_CELL_LENGTH << " "\
+									 << min_y + j * CELL_LENGTH + q * SMALL_CELL_LENGTH << endl;
+							} else {
+								break;
+							}
+						}
+					}
+
+				} else if (direction == 3) {
+					// from buttom to top
+					for (int p = 0; p < size; p++) {
+						for (int q = 0; q < size; q++) {
+							if (exist[p][q] == false) {
+								cout << min_x + i * CELL_LENGTH + p * SMALL_CELL_LENGTH << " "\
+									 << min_y + j * CELL_LENGTH + q * SMALL_CELL_LENGTH << endl;
+							} else {
+								break;
+							}
+						}
+					}
+					
+				} else {	// direction == 4      0 is impossable here
+					for (int q = size - 1; q >= 0; q--) {
+						for (int p = 0; p < size; p++) {
+							if (exist[p][q] == false) {
+								cout << min_x + i * CELL_LENGTH + p * SMALL_CELL_LENGTH << " "\
+									 << min_y + j * CELL_LENGTH + q * SMALL_CELL_LENGTH << endl;
+							} else {
+								break;
+							}
+						}
+					}
+				}
+
+				// look for furthest point from inside to boundary
+			}
+		}
+	}
+
+
+
 	
 	return 0;
 }
